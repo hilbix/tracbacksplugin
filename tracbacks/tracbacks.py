403fd4984e1b40f8a03c2485032f523c477ef734
Valentin Hilbig <webmaster@scylla-charybdis.com>
2024-02-25 15:26:21 +0000

    Works for ticket:XXX and blocks
    The complete logic was mostly rewritten.
    
    - blocks and text in backticks are now properly ignored
    - Excerpts for ticket:XXX are now generated, too
    
    Also I hope the code is much cleaner now.

--- tracbacks/tracbacks.py
+++ tracbacks/tracbacks.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2008 The Open Planning Project
+# Lines marked with #tino are NOT copyrighted.
 #
 # All rights reserved.
 #
@@ -28,25 +29,16 @@ class TracBacksPlugin(Component):
     TRACBACK_MAGIC_NUMBER = "{{{\n#!html\n<div class=\"tracback\"></div>\n}}}\n"
     TRACBACK_PREFIX = "This ticket has been referenced in ticket #"
 
-    TICKET_REGEX = r"""
-        (?=                    # Don't return '#' character:
-          (?!\{\{\{.*)	       # Exclude comment blocks
-	  (?<=^\#)            # Look for a TracLink Ticket at the beginning of the string
-          |(?<=[\s,.;:!]\#)    # or on a whitespace boundary or some punctuation
-          |(?<=ticket:)        # or the "ticket:NNN" format
-        )
-        (?!.*\}\}\})
-        (\d+)                  # Any length ticket number (return the digits)
-        (?=
-           (?=\b)              # Don't return word boundary at the end
-          |$                   # Don't return end of string
-        )
-        """
+    RE_TICKET = r"(?:^|\b|\s)(?:[#]|ticket:)(\d+)(?:\s|\b|$)"	#tino
+    RE_DACT_1 = r"(\{\{\{((?!\{\{\{).)*?\}\}\})"		#tino
+    RE_DACT_2 = r"`.*?`"					#tino
+
+    re_0 = re.compile(RE_TICKET, re.DOTALL|re.VERBOSE)		#tino
+    re_1 = re.compile(RE_DACT_1, re.DOTALL|re.VERBOSE)		#tino
+    re_2 = re.compile(RE_DACT_2, re.DOTALL|re.VERBOSE)		#tino
 
     EXCERPT_CHARACTERS = 80
     WEED_BUFFER = 2
-
-    # ITicketChangeListener methods
 
     def ticket_created(self, ticket):
         # Check for tracbacks on ticket creation.
@@ -55,27 +47,38 @@ class TracBacksPlugin(Component):
 
     def ticket_changed(self, ticket, comment, author, old_values):
 
-        pattern = re.compile(self.TICKET_REGEX, re.DOTALL|re.VERBOSE)
-
         if not isinstance(comment, basestring):
             return
 
-        tickets_referenced = pattern.findall(comment)
+        redacted	= self.redact(self.re_1, comment)	#tino
+        redacted	= self.redact(self.re_2, redacted)	#tino
+        tickets_referenced = self.re_0.findall(redacted)
         # convert from strings to ints and discard duplicates
         tickets_referenced = set(int(t) for t in tickets_referenced)
         # remove possible self-reference
         tickets_referenced.discard(ticket.id)
 
         # put trackbacks on the tickets that we found
-        if not self.is_tracback(comment): # prevent infinite recursion
-            for ticket_to_tracback in tickets_referenced:
-                try:
-                    t = Ticket(self.env, ticket_to_tracback)
-                except ResourceNotFound: # referenced ticket does not exist
-                    continue
+        if self.is_tracback(comment): # prevent infinite recursion
+            return
+        for ticket_to_tracback in tickets_referenced:
+            try:
+                t = Ticket(self.env, ticket_to_tracback)
+            except ResourceNotFound: # referenced ticket does not exist
+                continue
 
-                tracback = self.create_tracbacks(ticket, t, comment)
-                t.save_changes(author, tracback)
+            tracback = self.create_tracbacks(ticket, t, comment, redacted)
+            t.save_changes(author, tracback)
+
+    #tino replace everything we do not want with spaces
+    #tino this keeps the offsets in sync with the original
+    def redact(self,re,s):					#tino
+        while True:						#tino
+            m   = re.search(s)					#tino
+            if not m: return s					#tino
+            x   = m.start(0)					#tino
+            y   = m.end(0)					#tino
+            s   = s[0:x] + ' ' * (y-x) + s[y:]			#tino
 
     def ticket_deleted(self, ticket):
         pass
@@ -83,40 +86,37 @@ class TracBacksPlugin(Component):
     def is_tracback(self, comment):
         return comment.startswith(self.TRACBACK_MAGIC_NUMBER)
 
-    def create_tracbacks(self, ticket, ticket_to_tracback, comment):
-        tracback = self.TRACBACK_MAGIC_NUMBER + self.TRACBACK_PREFIX + str(ticket.id) + ":"
+    #tino get the position of all #id or ticket:id references
+    def find_ref(self, comment, id):				#tino
+        id = str(id)						#tino
+        for m in self.re_0.finditer(comment):			#tino
+            if m.group(1) == id:				#tino
+                yield [m.start(1),len(m.group(0))]		#tino
 
-        # find all occurrences of ticket_to_tracback. This is error prone.
-        # we'll weed the errors out later.
-        string_representation = "#" + str(ticket_to_tracback.id)
+    def create_tracbacks(self, ticket, ticket_to_tracback, comment, redacted):
+        tracback = self.TRACBACK_MAGIC_NUMBER + self.TRACBACK_PREFIX + str(ticket.id) + ":"
 
         excerpts = []
 
         index = -1
-        while comment.find(string_representation, index + 1) > -1:
-            # Get two characters in context so we can make sure this is really
-            # a reference to a ticket, and not anything else.
-            index = comment.find(string_representation, index + 1)
+        for [index,more] in self.find_ref(comment, ticket_to_tracback.id):	#tino
 
-            if not self.is_weed(comment, index, index + len(string_representation)):
                 start = index - self.EXCERPT_CHARACTERS
-                end = index + len(string_representation) + self.EXCERPT_CHARACTERS
+                end = index + more + self.EXCERPT_CHARACTERS
 
                 left_ellipsis = "..."
                 right_ellipsis = "..."
 
-                # Make sure we don't go into the negative. Also, make the ellipsis'
-                # disappear if we're not actually cutting up the comment.
-                if start <= 0:
+                if start <= 2:					#tino
                     left_ellipsis = ""
                     start = 0
 
-                if end >= len(comment):
+                if end >= len(comment)-2:			#tino
                     right_ellipsis = ""
 
                 excerpt = comment[start:end]
-                excerpt = excerpt.replace("\n", "")
-                excerpt = excerpt.replace("\r", "")
+                excerpt = excerpt.replace("\n", ' ')		#tino
+                excerpt = excerpt.replace("\r", ' ')		#tino
 
                 # There's probably a better way to say this in python, but Tim doesn't know
                 # how to do it. (He's tried """ but something's foobar'ed.)
@@ -124,18 +124,4 @@ class TracBacksPlugin(Component):
 
         tracback += ''.join(excerpts)
         return tracback
-
-    def is_weed(self, comment, start, end):
-        start -= self.WEED_BUFFER
-        end += self.WEED_BUFFER
-
-        # Make sure we don't have a negative starting value.
-        if start < 0:
-            start = 0
-
-        try:
-            match = re.search(self.TICKET_REGEX, comment[start:end])
-            return False
-        except: # Not a match. This must be a weed.
-            return True
 
